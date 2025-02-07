61a56c56f9b2ccff87fd61017a0ea98039d5fa03
wzambrano <williamzr-001@udenar.edu.co>
2025-02-06 22:06:16 -0500

    iconos en Dashboard

--- front-lava-autos/src/app/features/dashboard/main/main.component.ts
+++ front-lava-autos/src/app/features/dashboard/main/main.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  constructor(
+    private router: Router
+  ){}
+
+  goToServices(){
+    this.router.navigate(['list-services']);
+  }
 
 }

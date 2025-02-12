14f5d0e32b28539c2467a9471509758fd33cdf09
Carlos Ortiz G <carlos.eog97@gmail.com>
2025-02-12 00:44:20 -0500

    Implement employee form with validation and edit functionality

--- front-lava-autos/src/app/features/employees/employee-form/employee-form.component.ts
+++ front-lava-autos/src/app/features/employees/employee-form/employee-form.component.ts
@@ -1,11 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeesService } from '@services/employees.service';
+import { IEmployee } from 'app/shared/interfaces/employee';
+import { ButtonModule } from 'primeng/button';
+import { DatePicker } from 'primeng/datepicker';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'employee-form',
-  imports: [],
+  imports: [ReactiveFormsModule, FloatLabel, InputTextModule, DatePicker, ButtonModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.scss'
 })
 export default class EmployeeFormComponent {
 
+  private fb = inject(FormBuilder);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router)
+  private employeesService = inject(EmployeesService);
+
+  editMode = false;
+  loading = true;
+  originalData: any;
+
+  employeeForm: FormGroup = this.fb.group({
+    cedula: ['', Validators.required],
+    nombre: ['', Validators.required],
+    apellido: ['', Validators.required],
+    contacto: ['', Validators.required],
+    direccion: ['', Validators.required],
+    fecha_nacimiento: ['', Validators.required],
+    porcentaje_servicio: ['', [Validators.required, Validators.min(0), Validators.max(100)]]
+  });
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params && params['cedula']) {
+        this.editMode = true;
+        this.originalData = params;
+        this.employeeForm.patchValue(params);
+      } else {
+        this.editMode = false;
+      }
+      this.loading = false;
+    });
+  }
+
+  onSubmit() {
+    if (this.employeeForm.valid) {
+      console.log(this.employeeForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  private getFormValue() {
+    return {
+      ...this.employeeForm.value,
+      cedula: this.originalData?.cedula || this.employeeForm.value.cedula
+    }
+  }
+
+  // private handleSuccess() {
+  //   this.router.navigate(['/employees']);
+  // }
+
+  edit() {
+    console.log(this.getFormValue());
+    
+    this.getFormValue();
+  }
+
+  save() {}
+
+  cancel() {
+    this.router.navigate(['/employees']);
+  }
 }

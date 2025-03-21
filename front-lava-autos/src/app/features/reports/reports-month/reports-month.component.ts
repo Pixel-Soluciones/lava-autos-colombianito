6383d3ec867af93fab87eeca80298c7846849362
Carlos Ortiz G <carlos.eog97@gmail.com>
2025-03-20 23:10:05 -0500

    add route month report

--- front-lava-autos/src/app/features/reports/reports-month/reports-month.component.ts
+++ front-lava-autos/src/app/features/reports/reports-month/reports-month.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './reports-month.component.html',
   styleUrl: './reports-month.component.scss'
 })
-export class ReportsMonthComponent {
+export default class ReportsMonthComponent {
 
 }

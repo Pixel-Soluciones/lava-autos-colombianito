fae843837e61a57ca693c8b9fc29439e8f96591e
wzambrano <williamzr-001@udenar.edu.co>
2025-03-22 20:49:04 -0500

    reports form

--- front-lava-autos/src/app/features/reports/reports-day/reports-day.component.ts
+++ front-lava-autos/src/app/features/reports/reports-day/reports-day.component.ts
@@ -1,22 +1,72 @@
 import { Component } from '@angular/core';
-import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { IEmployee } from 'app/shared/interfaces/employee';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DatePicker } from 'primeng/datepicker';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { SelectChangeEvent, SelectModule } from 'primeng/select';
 import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-reports-day',
-  imports: [FormsModule, DatePicker, FloatLabel, CardModule, ReactiveFormsModule, InputTextModule, TableModule],
+  imports: [
+    FormsModule,
+    DatePicker,
+    FloatLabel,
+    CardModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    TableModule,
+    SelectModule,
+    ButtonModule
+  ],
   templateUrl: './reports-day.component.html',
-  styleUrl: './reports-day.component.scss'
+  styleUrl: './reports-day.component.scss',
 })
 export class ReportsDayComponent {
-fecha_string: string = '';
+  fecha_string: string = '';
+  employees: IEmployee[] = [];
+  ingresosdia: any[] = [];
+  serviciosEmployee: any[] = [];
 
-dateForm = new FormGroup({
+  dateForm = new FormGroup({
     fecha: new FormControl<string>('', Validators.required),
   });
+
+  employeeForm = new FormGroup({
+    employee: new FormControl<IEmployee | null>(null, Validators.required),
+  });
+
+  setDate(event: any) {
+    const fecha = new Date(event);
+
+    const opcionesDia: Intl.DateTimeFormatOptions = { weekday: 'long' };
+    const diaSemana = fecha.toLocaleDateString('es-ES', opcionesDia);
+    const diaCapitalizado =
+      diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
+
+    const opcionesFecha: Intl.DateTimeFormatOptions = {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    };
+    const fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha);
+
+    this.fecha_string = `${diaCapitalizado}, ${fechaFormateada}`;
+  }
+
+  setEmployee(event: SelectChangeEvent) {}
+
+  exportReport(){}
+
+  salir(){}
 
 }

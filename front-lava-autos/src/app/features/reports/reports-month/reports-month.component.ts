1bd1ed2750d4e455a75a7603e4b8ad3e0473e710
Carlos Ortiz G <carlos.eog97@gmail.com>
2025-03-24 13:55:47 -0500

    feat(monthlyReport): update data report

--- front-lava-autos/src/app/features/reports/reports-month/reports-month.component.ts
+++ front-lava-autos/src/app/features/reports/reports-month/reports-month.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
@@ -7,6 +8,7 @@ import { FloatLabel } from 'primeng/floatlabel';
 import { TableReportComponent } from "../../../shared/components/table-report/table-report.component";
 import { getEntityPropiedades } from 'app/shared/models/columnsTables';
 import { MonthlyReportService } from './services/monthly-report.service';
+import { generateExcelReport } from 'app/shared/utils/generateExcelReport';
 
 export interface monthSelected {
   month: number;
@@ -15,7 +17,7 @@ export interface monthSelected {
 
 @Component({
   selector: 'app-reports-month',
-  imports: [FloatLabel, DatePicker, FormsModule, ButtonModule, TableReportComponent],
+  imports: [FloatLabel, DatePicker, FormsModule, ButtonModule, TableReportComponent, CommonModule],
   templateUrl: './reports-month.component.html',
   styleUrl: './reports-month.component.scss'
 })
@@ -28,12 +30,27 @@ export default class ReportsMonthComponent {
   cols: any[] = [];
   services: any[] = [];
 
+  total: number = 0;
+  total_trabajadores: number = 0;
+  total_ganancias: number = 0;
+  servicioMasSolicitado: string = '';
+
   selectedMonth(date: Date) {
     if (date) {
       const formattedDate = this.formatDate(date);
       this.#monthlyReportService.get(formattedDate).subscribe((response) => {
         this.services = response;
         this.cols = getEntityPropiedades('monthlyReport');
+
+        this.total = response.reduce((acc, service) => acc + service.valor, 0);
+        this.total_trabajadores = response.reduce((acc, service) => acc + service.valor_porcentaje, 0);
+        this.total_ganancias = this.total - this.total_trabajadores;
+        this.servicioMasSolicitado = response
+          .map(item => item.servicio)
+          .reduce((acc, curr, _, arr) => {
+            const count = arr.filter(item => item === curr).length;
+            return acc.count < count ? { value: curr, count } : acc;
+          }, { value: '', count: 0 }).value;
       });
     }
   }
@@ -58,7 +75,7 @@ export default class ReportsMonthComponent {
       worksheet.push(row);
     });
 
-    console.log("📄", worksheet);
+    generateExcelReport(worksheet);
   }
 
   salir() {

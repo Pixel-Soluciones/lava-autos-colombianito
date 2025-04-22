792d7e785a44a8fcf914d471e2b41c92c69280b5
Carlos Ortiz G <carlos.eog97@gmail.com>
2025-04-22 15:25:12 -0500

    fix(employee-form): add max limit and validation for porcentaje_servicio field

--- front-lava-autos/src/app/features/employees/employee-form/employee-form.component.ts
+++ front-lava-autos/src/app/features/employees/employee-form/employee-form.component.ts
@@ -155,6 +155,12 @@ export default class EmployeeFormComponent implements OnInit {
             this.#router.navigate(['/employees']);
           }
         });
-    
+  }
+
+  limitPercentage(): void {
+    const value = this.employeeForm.get('porcentaje_servicio')?.value;
+    if (value > 100) {
+      this.employeeForm.get('porcentaje_servicio')?.setValue(100);
+    }
   }
 }
